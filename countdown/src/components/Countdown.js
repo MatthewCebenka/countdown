6025131bc36e62fd15b07885a06795d540cf5f53
Matthew Cebenka <matthewt.cebenka@gmail.com>
2019-12-09 17:44:43 -0500

    Added State Managment and mounting hooks for the time

--- countdown/src/components/Countdown.js
+++ countdown/src/components/Countdown.js
@@ -1,22 +1,39 @@
 import React, { Component } from "react";
+import moment from "moment";
 
 export default class Countdown extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      interval: this.getRemainingTime()
+      duration: this.getRemainingTime()
     };
   }
 
-  getRemainingTime() {
-    let now = new Date,
-    newYear = new Date(now.getFullYear() + 1, 0, 1)
 
-    console.log(now, newYear)
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({
+        duration: this.getRemainingTime()
+      })
+    }, 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
+
+  getRemainingTime() {
+    let now = moment(),
+      newYear = moment({ year: now.year() + 1 }),
+      diff = newYear.diff(now);
+
+    return moment.duration(diff);
   }
 
   render() {
+    const duration = this.state.duration;
+
     return (
       <section className="hero is-info is-fullheight has-text-centered">
         <div className="hero-body">
@@ -27,25 +44,25 @@ export default class Countdown extends Component {
                 <div className="level-item has-text-centered">
                   <div>
                     <p className="heading">Days</p>
-                    <p className="title">3,456</p>
+                    <p className="title">{Math.floor(duration.asDays())}</p>
                   </div>
                 </div>
                 <div className="level-item has-text-centered">
                   <div>
                     <p className="heading">Hours</p>
-                    <p className="title">123</p>
+                    <p className="title">{duration.hours().toString().padStart(2, '0')}</p>
                   </div>
                 </div>
                 <div className="level-item has-text-centered">
                   <div>
                     <p className="heading">Minutes</p>
-                    <p className="title">456K</p>
+                    <p className="title">{duration.minutes().toString().padStart(2, '0')}</p>
                   </div>
                 </div>
                 <div className="level-item has-text-centered">
                   <div>
                     <p className="heading">Seconds</p>
-                    <p className="title">789</p>
+                    <p className="title">{duration.seconds().toString().padStart(2, '0')}</p>
                   </div>
                 </div>
               </nav>
