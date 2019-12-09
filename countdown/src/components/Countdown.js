5541a8d657cd172c25605ac2e3d9afcf1bdbe085
Matthew Cebenka <matthewt.cebenka@gmail.com>
2019-12-09 11:18:16 -0500

    converted countdown component to class component

--- countdown/src/components/Countdown.js
+++ countdown/src/components/Countdown.js
@@ -1,15 +1,58 @@
-import React from "react";
+import React, { Component } from "react";
 
-function Countdown(props) {
-  return (
-    <section className="hero is-info is-fullheight">
-      <div className="hero-body">
-        <div className="container">
-          <h1 className="title">New Year is Coming!</h1>
+export default class Countdown extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      interval: this.getRemainingTime()
+    };
+  }
+
+  getRemainingTime() {
+    let now = new Date,
+    newYear = new Date(now.getFullYear() + 1, 0, 1)
+
+    console.log(now, newYear)
+  }
+
+  render() {
+    return (
+      <section className="hero is-info is-fullheight has-text-centered">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">New Year is Coming!</h1>
+            <div className="section">
+              <nav className="level">
+                <div className="level-item has-text-centered">
+                  <div>
+                    <p className="heading">Days</p>
+                    <p className="title">3,456</p>
+                  </div>
+                </div>
+                <div className="level-item has-text-centered">
+                  <div>
+                    <p className="heading">Hours</p>
+                    <p className="title">123</p>
+                  </div>
+                </div>
+                <div className="level-item has-text-centered">
+                  <div>
+                    <p className="heading">Minutes</p>
+                    <p className="title">456K</p>
+                  </div>
+                </div>
+                <div className="level-item has-text-centered">
+                  <div>
+                    <p className="heading">Seconds</p>
+                    <p className="title">789</p>
+                  </div>
+                </div>
+              </nav>
+            </div>
+          </div>
         </div>
-      </div>
-    </section>
-  );
+      </section>
+    );
+  }
 }
-
-export default Countdown;
