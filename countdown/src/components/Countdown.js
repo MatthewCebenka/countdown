eda3945a81733013e5fc8fbef767c849be9b7105
Matthew Cebenka <matthewt.cebenka@gmail.com>
2019-12-09 10:41:47 -0500

    created react app and optimized file structure

---
+++ countdown/src/components/Countdown.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Countdown(props) {
+  return (
+    <section className="hero is-info is-fullheight">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">New Year is Coming!</h1>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Countdown;
